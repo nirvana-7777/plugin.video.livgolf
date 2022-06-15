1d31c5021dd154f069c427e7be81cc63d6ff772e
nirvana-7777 <nirv_inc@yahoo.com>
2022-06-15 13:48:39 +0200

    Initial Release

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def play_video(videoid):
     duration = video_details['video']['gist']['runtime']
     aired = video_details['video']['gist']['publishDate']
     language = video_details['video']['gist']['languageCode']
-    print(plugin.timestamp(aired))
+    print(plugin.time_stamp(aired))
     print('Language' + language)
     metadata = {'plot': description,
                 'aired': aired,
